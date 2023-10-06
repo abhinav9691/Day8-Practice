5b3462fd1ae8fbdc12e68fb3e23ed29e4a8707b4
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-06 23:34:54 +0530

    [Abhinav]-adding String part

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Day8_Practice;
+Console.WriteLine("1)Enter String \n2)LowerCase Alphabet \n3)Html Tags \n4)Number Of Occurences \n5)Image File Extension \n6)Website Address");
+int option = int.Parse(Console.ReadLine());
+while (true)
+{
+    switch (option)
+    {
+        case 1:
+            Strin str1 = new Strin();
+            str1.checkstring();
+            break;
+        
+    }
+}
+
---
+++ Strin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8_Practice
+{
+    internal class Strin
+    {
+        public void checkstring()
+        {
+            Console.WriteLine("Enter string : ");
+            string s = Console.ReadLine();
+            string pattern1 = "^[a]{1}[b]{2,3}$";
+            Regex regex = new Regex(pattern1);
+            if (regex.IsMatch(s))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid String");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid String");
+                Console.ResetColor();
+            }
+        }
+    }
+}
