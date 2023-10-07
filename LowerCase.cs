9f57b306caed8130a0158e3057d8751501a0fcda
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-07 08:31:32 +0530

    [Abhinav]-adding Lowecase part

---
+++ LowerCase.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+internal class LowerCase
+{
+    public void CheckLowerCase()
+    {
+        Console.WriteLine("Enter LowerCase : ");
+        string s1 = Console.ReadLine();
+        string pattern2 = "^[a-z]{4,}[_]{1}$";
+        Regex regex1 = new Regex(pattern2);
+        if (regex1.IsMatch(s1))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Valid LowerCase");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid LowerCase");
+            Console.ResetColor();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,14 @@ while (true)
             Strin str1 = new Strin();
             str1.checkstring();
             break;
-        
+        case 2:
+            LowerCase lower = new LowerCase();
+            lower.CheckLowerCase();
+            break;
+        case 3:
+            HtmlTags html = new HtmlTags();
+            html.CheckHtmlTags();
+            break;
     }
 }
 
